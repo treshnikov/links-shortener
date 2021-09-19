4a08909f4aaf1b3b39c6fddb32f6c7fe0d922b89
treshnikov <treshnikov@gmail.com>
2021-09-19 23:31:21 +0400

    Add http hook

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,6 +1,26 @@
-import React from "react"
+import React, {useContext, useState } from "react"
+import {useHttp} from '../hooks/http.hooks'
 
 export const AuthPage = () => {
+    const {loading, error, request} = useHttp()
+
+    const [form, setForm] = useState({
+        email: '', password: ''
+    })
+
+    const changeHandler = event => {
+        setForm({...form, [event.target.name]: event.target.value })
+    }
+
+    const registerHandler = async () => {
+        try {
+            const data = await request('/api/auth/register', 'POST', {...form})
+            console.log('data', data)
+        } catch (e) {
+            
+        }
+    }
+
     return (
         <div className="row">
             <div className="col s6 offset-s3">
@@ -10,30 +30,41 @@ export const AuthPage = () => {
                         <div className="card-content white-text">
                             <span className="card-title" style={{ color: "black" }}>Authentication</span>
                             <div>
-                                <div class="input-field">
+                                <div className="input-field">
                                     <input
-                                        placeholder="Your email address"
                                         id="inputEmail"
-                                        type="text"
-                                        name="inputEmail"
-                                        class="email" />
+                                        type="email"
+                                        name="email"
+                                        onChange={changeHandler}
+                                        className="validate" />
                                     <label for="inputEmail">Email</label>
                                 </div>
 
-                                <div class="input-field">
+                                <div className="input-field">
                                     <input
-                                        placeholder="Your password"
                                         id="inputPassword"
                                         type="password"
-                                        name="inputPassword"
-                                        class="password" />
+                                        name="password"
+                                        onChange={changeHandler}
+                                        className="validate"/>
                                     <label for="inputPassword">Password</label>
                                 </div>
                             </div>
                         </div>
                         <div className="card-action">
-                            <input type="button" className="btn teal lighten-2" style={{ marginRight: 10 }} value="Login" />
-                            <input type="button" className="btn grey lighten-1 black-text" value="Register" />
+                            <input 
+                                type="button" 
+                                className="btn teal lighten-2" 
+                                style={{ marginRight: 10 }} 
+                                value="Login"
+                                disabled={loading}/>
+                            <input 
+                                type="button" 
+                                className="btn grey lighten-1 black-text" 
+                                value="Register" 
+                                onClick={registerHandler}
+                                disabled={loading}
+                                />
                         </div>
                     </div>
                 </div>
