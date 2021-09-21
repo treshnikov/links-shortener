31b49fd71df3097978a997947a680cd7799a2a68
treshnikov <treshnikov@gmail.com>
2021-09-21 17:12:47 +0400

    Add links list

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { useRoutes } from './routes'
 import { useAuth } from './hooks/auth.hook'
 import { AuthContext } from './context/AuthContext'
 import {Navbar} from './components/Navbar'
+import {Loader} from './components/Loader'
 
 function App() {
   const { token, login, logout, userId } = useAuth()
--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -1,46 +1,62 @@
-import React, {useContext, useEffect, useState} from 'react'
-import {useHttp} from '../hooks/http.hook'
-import {AuthContext} from '../context/AuthContext'
-import {useHistory} from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react'
+import { useHttp } from '../hooks/http.hook'
+import { AuthContext } from '../context/AuthContext'
+import { useHistory } from 'react-router-dom'
 
 export const CreatePage = () => {
-  const history = useHistory()
-  const auth = useContext(AuthContext)
-  const {request} = useHttp()
-  const [link, setLink] = useState('')
+    const history = useHistory()
+    const auth = useContext(AuthContext)
+    const { request, loading } = useHttp()
+    const [link, setLink] = useState('')
 
-  useEffect(() => {
-    window.M.updateTextFields()
-  }, [])
+    useEffect(() => {
+        window.M.updateTextFields()
+    }, [])
 
-  const pressHandler = async event => {
-    if (event.key === 'Enter') {
-      try {
-        console.log(111)
-        const data = await request('/api/link/generate', 'POST', {from: link}, {
-          Authorization: `Bearer ${auth.token}`
-        })
-        console.log(data)
-        history.push(`/detail/${data.link._id}`)
-      } catch (e) {}
+    const pressHandler = async event => {
+        if (event.key === 'Enter') {
+            await handleCreate()
+        }
     }
-  }
 
-  return (
-    <div className="row">
-      <div className="col s8 offset-s2" style={{paddingTop: '2rem'}}>
-        <div className="input-field">
-          <input
-            placeholder="Link"
-            id="link"
-            type="text"
-            value={link}
-            onChange={e => setLink(e.target.value)}
-            onKeyPress={pressHandler}
-          />
-          <label htmlFor="link">Type your link here</label>
-        </div>
-      </div>
-    </div>
-  )
+    const handleCreate = async () =>{
+        try {
+            const data = await request('/api/link/generate', 'POST', { from: link }, {
+                Authorization: `Bearer ${auth.token}`
+            })
+            console.log(data)
+            history.push(`/detail/${data.link._id}`)
+        } catch (e) { }
+
+    }
+
+    return (
+        <>
+            <div className="row">
+                <div className="col s8 offset-s2" style={{ paddingTop: '2rem' }}>
+                    <div className="input-field">
+                        <input
+                            placeholder="Link"
+                            id="link"
+                            type="text"
+                            value={link}
+                            onChange={e => setLink(e.target.value)}
+                            onKeyPress={pressHandler}
+                        />
+                        <label htmlFor="link">Type your link here</label>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col s8 offset-s2">
+                    <input
+                        type="button"
+                        className="btn"
+                        value="Create"
+                        onClick={handleCreate}
+                        disabled={loading}/>
+                </div>
+            </div>
+        </>
+    )
 }
